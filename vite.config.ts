c3f4b059446ef4485930b734687b5256373ff53b
jobrunner95 <johannes_brunner95@web.de>
2025-08-17 15:28:44 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Es ist keine weitere Konfiguration für diese App erforderlich.
+  // Vite behandelt die meisten Dinge, einschließlich Umgebungsvariablen wie import.meta.env.VITE_API_KEY, automatisch.
+});
